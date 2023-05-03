c40a351a82b76a10f722eafcee88e45c3823b71b
muzscripter <109730545+muzscripter@users.noreply.github.com>
2023-05-02 21:22:06 -0400

    Create lib.rs

---
+++ src/lib.rs
@@ -0,0 +1,27 @@
+macro_rules! game {
+    () -> {
+        nusion_core::env!()
+            .modules()
+            .find_executable_file_name("Roblox.exe")
+            .unwrap();
+    }
+}
+
+macro_rules! game_mut {
+    () -> {
+        nusion_core::env_mut!()
+            .modules_mut()
+            .find_executable_file_name("Roblox.exe")
+            .unwrap();
+    }
+}
+
+use nusion_core::patch:(Patch, Reader, Writer); // Unused Imports
+
+#[nusion_core::main("Roblox.exe")]
+fn main() -> Outcome<(), Box<dyn std::error::Error>> {
+    
+    game!().patch_read();
+
+    return Outcome(());
+}
